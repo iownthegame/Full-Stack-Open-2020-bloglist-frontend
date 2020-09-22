0044aa0d1ce77f423c5ade8956fe8c94b42de2df
Huiyu <iownthegame@gmail.com>
2020-09-23 01:22:59 +0200

    ex 5.5 - add Togglable

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 
 import './App.css';
 
@@ -156,7 +157,9 @@ const App = () => {
 
       <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
 
-      {createNewForm()}
+      <Togglable buttonLabel="new note">
+        {createNewForm()}
+      </Togglable>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
